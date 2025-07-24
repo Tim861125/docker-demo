af765bccd0bffad4e6b02068157d12e1d53513ad
wuxin.ding <wuxin.ding@innovue.ltd>
2025-07-24 15:34:00 +0800

    Update

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.question("請輸入你的名字: ", (name: string) => {
+	console.log(`你好，${name}`);
+	rl.close();
+});
